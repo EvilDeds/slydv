025311e9804608f9f42e311c1b27ea35d0f0b6f4
India Amos <india@indiamos.com>
2017-08-11 15:53:40 -0400

    FEAT(repl): First pass on repl slide creation form

---
+++ client/components/ReplSlideForm.js
@@ -0,0 +1,56 @@
+// This is a dumb form component, meant to be called by a higher-order component that will supply a handleSubmit and, if the form is being used to edit an existing record, that record's previous values.
+
+import React from 'react';
+import {connect} from 'react-redux';
+
+export default ReplSlideForm;
+
+/* -------------- COMPONENT -------------- */
+
+const ReplSlideForm = (props) => {
+  // console.log('form props: ', props);
+  const {error, handleChange, handleSubmit} = props;
+
+  return (
+    <div className="ReplSlideForm col-lg-9 offset-lg-3">
+      <form onSubmit={handleSubmit} className="form-horizontal">
+        <div className="form-group">
+          <label className="col-md-4 control-label" htmlFor="slideTitle">* Slide title</label>
+          <div className="col-md-5">
+          <input id="slideTitle" name="slideTitle" type="text" className="form-control input-md">
+          <span className="help-block">A slide title is optional</span>
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label className="col-md-4 control-label" htmlFor="initialCode">Initial code</label>
+          <div className="col-md-4">
+            <textarea className="form-control" id="initialCode" name="initialCode"></textarea>
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label className="col-md-4 control-label" htmlFor="speakerNotes">Speaker Notes</label>
+          <div className="col-md-4">
+            <textarea className="form-control" id="speakerNotes" name="speakerNotes"></textarea>
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label className="col-md-4 control-label" for="SaveAndNew" type="submit">Save + New</label>
+          <div className="col-md-8">
+            <button id="SaveAndNew" name="SaveAndNew" className="btn btn-success">Save + New</button>
+            <button id="ClearForm" name="ClearForm" className="btn btn-danger">Clear Form</button>
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label className="col-md-4 control-label" for="textify">Textify</label>
+          <div className="col-md-4">
+            <button id="textify" name="textify" className="btn btn-primary">Textify</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,7 @@ import {Router} from 'react-router'
 import {Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome} from './components'
+import {Main, Login, Signup, UserHome, ReplSlideForm} from './components'
 import {me} from './store'
 
 /**
@@ -23,6 +23,7 @@ class Routes extends Component {
         <Main>
           <Switch>
             {/* Routes placed here are available to all visitors */}
+            <Route path='/makeslide' component={ReplSlideForm} />
             <Route path='/login' component={Login} />
             <Route path='/signup' component={Signup} />
             {
