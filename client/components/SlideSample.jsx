b37fa7ea1c5a2c760f3510bb85f22a909a9660ad
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-08-15 15:04:30 -0400

    added in markdown support and created some markdown components for use in slides

--- client/components/SlideSample.jsx
+++ client/components/SlideSample.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { MarkdownHeader } from './Markdown';
 
 export default class SlideSample extends Component {
   // master layout
   render() {
     return (
       <div>
-        <header>header for Title Field or Site Header</header>
+        <MarkdownHeader markdown="for Title Field or Site Header" />
         <div id="main">
           <nav>nav for Sidebar in Edit Mode</nav>
           <section>section</section>
