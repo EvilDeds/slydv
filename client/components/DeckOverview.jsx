20e9f46be304f08f75c277835795dc4b669e340a
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-08-16 18:48:58 -0400

    fixed DeckOverview for linter

--- client/components/DeckOverview.jsx
+++ client/components/DeckOverview.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchDeck, fetchSlideList } from '../store';
 import { Link } from 'react-router-dom';
+import { fetchDeck, fetchSlideList } from '../store';
 
 class DeckOverview extends Component {
-  componentDidMount(nextProps) {
-    const deckId = +this.props.match.params.deckId
+  componentDidMount() {
+    const deckId = +this.props.match.params.deckId;
     this.props.loadDeck(deckId);
     this.props.loadSlides(deckId);
   }
@@ -14,41 +14,41 @@ class DeckOverview extends Component {
     const { deck, slides } = this.props;
     return (
       <div>
-      { slides[0] && deck.id
-        ? (
+        { slides[0] && deck.id
+          ? (
             <div>
               <h1>{ deck.deckTitle } </h1>
               <hr />
               {slides.map(slide => (
                 <div key={slide.id} className="deckview-slide-container">
                   <h2>{ slide.title }</h2>
-                  {/*FIX LINK WHEN WE HAVE EDIT PAGE*/}
-                  <Link to={``}>Edit Slide</Link>
+                  {/* FIX LINK WHEN WE HAVE EDIT PAGE */}
+                  <Link to={''}>Edit Slide</Link>
                 </div>
               ))}
             </div>
           )
-        : (
-          <h1>Deck not found</h1>
+          : (
+            <h1>Deck not found</h1>
           )
-      }
+        }
       </div>
-    )
-  };
+    );
+  }
 }
 
 const mapState = state => ({
   deck: state.deck,
-  slides: state.slide.slideList
-})
+  slides: state.slide.slideList,
+});
 
 const mapDispatch = dispatch => ({
   loadDeck(deckId) {
     dispatch(fetchDeck(deckId));
   },
-  loadSlides(deckId){
+  loadSlides(deckId) {
     dispatch(fetchSlideList(deckId));
-  }
-})
+  },
+});
 
-export default connect(mapState, mapDispatch)(DeckOverview)
+export default connect(mapState, mapDispatch)(DeckOverview);
