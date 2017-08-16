0f84ea5781419dd76944f69290599e5de853809b
Leigh Steiner <steinerleigh@gmail.com>
2017-08-16 17:16:50 -0400

    FEAT(Navbar): first pass on Navbar extraction, front end route update

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom';
 import { logout } from '../store';
 
+import Navbar from './Navbar';
 /* -------------- COMPONENT --------------
  *  The Main component is our 'picture frame' - it displays the navbar and anything
  *  else common to our entire app. The 'picture' inside the frame is the space
@@ -14,22 +15,7 @@ const Main = (props) => {
 
   return (
     <div>
-      <h1>SLYDV</h1>
-      <nav>
-        {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
-        }
-      </nav>
+      <Navbar />
       <hr />
       {children}
     </div>
