3912b781390bee743e243d0c801609f4d83db5c6
India Amos <india@indiamos.com>
2017-08-16 12:30:42 -0400

    Change react-a11y require to import

--- client/index.jsx
+++ client/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import a11y from 'react-a11y';
 import './index.scss';
 import store from './store';
 import Routes from './routes';
 
-const a11y = require('react-a11y');
 if (process.env.NODE_ENV !== 'production') a11y(React, { includeSrcNode: true });
 
 // establishes socket connection
