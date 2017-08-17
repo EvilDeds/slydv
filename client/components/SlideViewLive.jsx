4b8ef0c23630bbf64f287534c11dcb8ab6902b13
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-08-17 18:08:55 -0400

    hides navbar on slide view live

--- client/components/SlideViewLive.jsx
+++ client/components/SlideViewLive.jsx
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import SlideViewFrame from './SlideViewFrame';
-import { getSingleSlide, fetchSlideList, fetchDeck } from '../store';
+import { getSingleSlide, fetchSlideList, fetchDeck, viewNavBar } from '../store';
 
 class SlideViewLive extends Component {
   componentDidMount() {
-    console.log(this.props.currentSlide)
     const deckId = +this.props.match.params.deckId;
+    this.props.hideNavBar();
     if (!this.props.deck.id || deckId !== this.props.deck.id) this.props.loadDeck(deckId);
     if (!this.props.slides.length) this.props.loadSlides(deckId);
     if (!this.props.currentSlide.id) this.props.setSlide(this.props.slides[0]);
@@ -43,6 +43,9 @@ const mapDispatch = dispatch => ({
   loadDeck(deckId) {
     dispatch(fetchDeck(deckId));
   },
+  hideNavBar() {
+    dispatch(viewNavBar(false));
+  }
 });
 
 export default connect(mapState, mapDispatch) (SlideViewLive)
