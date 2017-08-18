a0b3fab12f9c86589f8ed4dd5d08945f349a687c
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-08-18 16:16:02 -0400

    runkit takes in code props

--- client/components/Runkit.jsx
+++ client/components/Runkit.jsx
@@ -1,38 +1,9 @@
 const React = require('react');
 const Embed = require('react-runkit');
 
-const helloSource = `function test(num){return 'Hi, '+num+1;}test(8);`;
-
-// class Runkit extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       code: 'console.log("Hello, world")',
-//     };
-//   }
-//
-//   render() {
-//     // const getSource = node => {
-//     //   setInterval(() => {
-//     //     var iframe = document.getElementById('iframeId');
-//     //    var innerDoc = iframe.contentDocument || iframe.contentWindow.document
-//     // was trying to add in a reference to the iframe to access the underlying
-//     // codemirror instance and use the codemirror values (or access the dom nodes)
-//     //   }), 1000)
-//     // } ref={getSource}
-//     return (
-//       <div>
-//         <Embed source={this.state.code} readOnly={false} onURLChanged={console.log} />
-//       </div>
-//     );
-//
-//
-//   }
-// }
-
-module.exports = class Runkit extends React.Component {
-  constructor() {
-    super();
+export default class Runkit extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
       code: '',
     };
@@ -43,15 +14,12 @@ module.exports = class Runkit extends React.Component {
   }
   alertEvaluated(event) {
     // (this.source and event are undefined, but we can update state in the callback below)
-    // console.log('hi', /* this.source, event */);
-    // console.log('getURL', this.refs.embed.getURL());
-    // console.log('getSource',this.refs.embed.getSource());
     this.refs.embed.getSource((code) => this.setState({ code }));
 
   }
 
   render() {
     console.log("CODE ON STATE",this.state.code);
-    return <Embed source={ helloSource } readOnly={ false } onEvaluate={this.alertEvaluated.bind(this)} ref="embed" />;
+    return <Embed source={ this.props.codeText  } readOnly={ false } onEvaluate={this.alertEvaluated.bind(this)} ref="embed" />;
   }
 };
