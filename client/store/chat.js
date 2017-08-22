fd12c29f081706410ac9f17709f8a1dfe0409291
Leigh Steiner <steinerleigh@gmail.com>
2017-08-21 20:54:36 -0400

    testing backend security

---
+++ client/store/chat.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const initialMessages = [];
+
+// action types
+const GET_MESSAGES = 'GET_MESSAGES';
+const GET_NEW_MESSAGE = 'GET_NEW_MESSAGE';
+
+// action creators
+const getMessages = (messages) => ({ type: GET_MESSAGES, messages });
+const getNewMessage = (message) => ({ type: GET_NEW_MESSAGE, message });
+
+// thunk creators
+
+export function fetchMessages(deckId) {
+  return function thunk(dispatch) {
+    return axios.get(`/api/decks/${deckId}/chats`)
+      .then(res => dispatch(getMessages(res.data)))
+      .catch((err) => { console.error(err) });
+  };
+}
+
+export function postMessage(deckId, userId, message){
+  return function thunk(dispatch){
+    return axios.post(`/api/decks/${deckId}/chats`, { deckId, userId, message})
+    .then(res => dispatch(getNewMessage(res.data)))
+    .catch((err) => { console.error(err)});
+  };
+}
+
+// reducer
+export default function (state = initialMessages, action) {
+  switch(action.type) {
+    case GET_MESSAGES:
+      return action.messages;
+    case GET_NEW_MESSAGE:
+      return [...state, action.message];
+    default:
+      return state;
+  }
+}
--- server/api/decks.js
+++ server/api/decks.js
@@ -3,17 +3,30 @@ const { Deck, Slide } = require('../db/models');
 
 router.get('/:deckId', (req, res, next) => {
   Deck.findById(+req.params.deckId, { include: [Slide] })
-    .then(deck => res.json(deck))
-    .catch(next);
-});
+    .then(deck => {
+    if(req.user.id === deck.userId || deck.isViewable){
+      res.json(deck)
+      .catch(next);
+    }else{
+     next(new Error('This deck is not for you!'))}
+  })
+);
 
 router.get('/:deckId/slides', (req, res, next) => {
-  Slide.findAll({
-    where: { deckId: +req.params.deckId },
-    order: [['positionInDeck', 'ASC']],
-  })
+  Deck.findById(+req.params.deckId)
+  .then(deck => {
+  	if(req.user.id === deck.userId || deck.isViewable){
+      Slide.findAll({
+      where: { deckId: +req.params.deckId },
+      order: [['positionInDeck', 'ASC']],
+      })
     .then(slides => res.json(slides))
     .catch(next);
-});
+  	}else{
+      next(new Error('These slides are not for you!'))
+  	}
+  })
+
+}); //the relevant security params here are on the deck not the slides, what should we do? 
 
 module.exports = router;
